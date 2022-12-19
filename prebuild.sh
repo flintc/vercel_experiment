3b3edc70b09335b56701913be9a7e2bcd8280db4
C Flint <flintc27@gmail.com>
2022-12-19 15:22:44 -0500

    update prebuild script to not use docker, but use openapi codegen web service instead

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
-
-
 response=$(curl -X POST -H "Content-Type: application/json" -d '{"openAPIUrl":"https://raw.githubusercontent.com/flintc/vercel_experiment/main/tmdb_spec.json"}' http://api.openapi-generator.tech/api/gen/clients/typescript-fetch)
 
 link=$(echo $response | sed -E 's/.*"link":"([^"]+)".*/\1/')
@@ -17,13 +15,4 @@ rm -rf generated/openapi-tmdb/
 mv openapi-tmdb generated/
 rm file.zip
 
-
-# docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-#     -i /local/tmdb_spec.json \
-#     -g typescript-fetch \
-#     -o /local/generated/openapi-tmdb
-
-# cp ./generated/openapi-tmdb/models/__edited__GETSearchMulti200Response.ts ./generated/openapi-tmdb/models/GETSearchMulti200Response.ts
-# cp ./generated/openapi-tmdb/models/__edited__GETTvTvIdSeasonSeasonNumber200Response.ts ./generated/openapi-tmdb/models/GETTvTvIdSeasonSeasonNumber200Response.ts
-
-# npx prisma generate
+npx prisma generate
